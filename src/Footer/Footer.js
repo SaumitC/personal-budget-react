2eebd88f736a542f5ad83c0d40716eb1ccbc7dc8
Saumit Chinchkhandi <saumitc@Saumits-MacBook-Pro.local>
2023-10-01 15:34:33 -0400

    Brought PBudget Code in - React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+    <div role="contentinfo" className="bottom">
+        <div className="center">
+            All rights reserved &copy; Saumit Chinchkhandi
+        </div>
+    </div>
+</footer>
+
+  );
+}
+
+export default Footer;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { 
+  Link
+  } from "react-router-dom";
+
+
+function Menu() {
+  return (
+    <nav>
+        <div className="menu">
+            <ul>
+                <li><Link to="/">Homepage</Link></li>
+                <li><Link to="./about">About</Link></li>
+                <li><Link to="./login">Login</Link></li>
+            </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default Menu;
